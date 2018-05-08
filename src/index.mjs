4767b0349476a2b10d1998da06015a2b33615c0c
Joe Fleming <joe.fleming@gmail.com>
2018-05-07 19:23:53 -0700

    feat: add validation methods to model

--- src/index.mjs
+++ src/index.mjs
@@ -7,52 +7,6 @@ const BELONGS_TO = 'BELONGS_TO';
 const modelRegistery = new Map();
 const ajv = new Ajv();
 
-function defineProp(obj, prop, config = {}) {
-  Object.defineProperty(obj, prop, {
-    enumerable: false,
-    writable: false,
-    configurable: false,
-    ...config,
-  });
-}
-
-function validateModel(child) {
-  function showError(msg) {
-    throw new Error(`Model failure, ${msg}: ${child.name}`);
-  }
-
-  // check for required definitions
-  if (!child.tableName) showError('`tableName` is required');
-}
-
-function validateRelationships(child, registry) {
-  function showError(msg, name) {
-    const err = `Relationship failure, ${msg}: ${child.name}`;
-    if (name) throw new Error(`${err} (relation \`${name}\`)`);
-    throw new Error(err);
-  }
-
-  if (!child.relationships) showError('Model has no relationships defined');
-
-  // check relationship definitions
-  if (child.relationships) {
-    Object.keys(child.relationships).forEach(name => {
-      const def = child.relationships[name];
-
-      // only a limited number of relationship types are supported
-      if ([HAS_MANY, BELONGS_TO].indexOf(def.relation) < 0)
-        showError(`Invalid relation type \`${def.relation}\``, name);
-
-      if (!registry.has(def.model)) showError(`Model \`${def.model}\` not in registry`, name);
-    });
-  }
-}
-
-function execute(child, method, ...args) {
-  if (typeof child[method] === 'function') return child[method](...args);
-  return null;
-}
-
 const joinMap = {
   inner: 'innerJoin',
   left: 'leftJoin',
@@ -63,10 +17,37 @@ const joinMap = {
   cross: 'crossJoin',
 };
 
+const relationshipSchema = {
+  type: 'object',
+  required: ['model'],
+  properties: {
+    model: { type: 'string' },
+    relation: { type: 'string', enum: [HAS_MANY, BELONGS_TO] },
+    joinType: { type: 'string', enum: Object.keys(joinMap) },
+    local: { type: 'string' },
+    remote: { type: 'string' },
+  },
+};
+
+function defineProp(obj, prop, config = {}) {
+  Object.defineProperty(obj, prop, {
+    enumerable: false,
+    writable: false,
+    configurable: false,
+    ...config,
+  });
+}
+
+function executeOnDef(child, method, ...args) {
+  if (typeof child[method] === 'function') return child[method](...args);
+  return null;
+}
+
 export default class BaseModel {
-  constructor(doc = {}) {
+  constructor(doc) {
     // validate model configuration
-    validateModel(this.constructor);
+    const { valid, errors } = this.constructor.isValid;
+    if (!valid) throw new Error(errors[0]);
 
     // register local properties
     defineProp(this, '$knex', { value: this.constructor.$knex });
@@ -74,39 +55,53 @@ export default class BaseModel {
     // keep the doc instance
     this.doc = doc;
 
-    execute(this.constructor, 'onCreate', this.doc);
+    executeOnDef(this.constructor, 'onCreate', this.doc);
   }
 
-  get primaryKey() {
-    // default primary key
-    return this.constructor.primaryKey || 'id';
+  static get isValid() {
+    const result = {
+      valid: true,
+      errors: [],
+    };
+
+    const addError = msg => {
+      result.valid = false;
+      result.errors.push(`Model failure, ${msg}: ${this.name}`);
+    };
+
+    // check for required definitions
+    if (!this.tableName) addError('`tableName` is required');
+
+    return result;
   }
 
-  async save() {
-    const jsonSchema = this.constructor.jsonSchema; // eslint-disable-line prefer-destructuring
-    const modifiedSchema = execute(this.constructor, 'beforeValidate', { ...jsonSchema }, this.doc);
-    const schema = modifiedSchema || jsonSchema;
+  static validateRelationships() {
+    const showError = msg => {
+      throw new Error(`Relationship error in ${this.name}: ${msg}`);
+    };
 
-    if (schema) {
-      const validate = ajv.compile(schema);
-      const valid = validate(this.doc);
-      if (!valid) {
-        // console.error(validate.errors); // TODO: proper error logger
-        throw new Error(
-          `document '${validate.errors[0].dataPath.replace(/^\./, '')}' ${
-            validate.errors[0].message
-          }`
-        );
+    // check model relationships
+    if (!this.relationships) showError('no relationships defined');
+
+    // check relationship definitions
+    Object.keys(this.relationships).forEach(name => {
+      const def = this.relationships[name];
+
+      // validate the relationship schema
+      if (!ajv.validate(relationshipSchema, def)) {
+        showError(`Invalid schema for \`${name}\`, ${ajv.errors[0].message.replace(/'/g, '`')}`);
       }
-    }
 
-    execute(this.constructor, 'beforeSave', this.doc);
+      // only a limited number of relationship types are supported
+      if ([HAS_MANY, BELONGS_TO].indexOf(def.relation) < 0) {
+        showError(`Invalid relation for \`${name}\`, \`${def.relation}\` `);
+      }
 
-    await this.$knex(this.constructor.tableName).insert(this.doc);
-
-    return this.$knex(this.constructor.tableName)
-      .where({ [this.primaryKey]: this.doc[this.primaryKey] })
-      .first();
+      // check that the model is actually in the registry
+      if (!this.registry.has(def.model)) {
+        showError(`Model \`${def.model}\` not in registry, for field \`${name}\` `);
+      }
+    });
   }
 
   static knex(value) {
@@ -118,24 +113,40 @@ export default class BaseModel {
     return 'id';
   }
 
+  static get hasMany() {
+    return HAS_MANY;
+  }
+
+  static get belongsTo() {
+    return BELONGS_TO;
+  }
+
+  // static get hasAndBelongsToMany() {
+  //   return HAS_AND_BELONGS_TO_MANY;
+  // }
+
   static register(registry = modelRegistery) {
+    // use optional external registry
+    this.registry = registry;
+
     // keep track of the model
-    if (registry.has(this.name)) throw new Error(`Model already registered: ${this.name}`);
-    registry.set(this.name, this);
+    if (this.registry.has(this.name)) throw new Error(`Model already registered: ${this.name}`);
+    this.registry.set(this.name, this);
   }
 
   static query() {
     return this.$knex(this.tableName);
   }
 
-  static queryWith(relations, registry = modelRegistery) {
+  static queryWith(relations) {
     /*
     knex.select('*').from('users').join('accounts', {'accounts.id': 'users.account_id'})
     Outputs:
     select * from `users` inner join `accounts` on `accounts`.`id` = `users`.`account_id`
     */
 
-    validateRelationships(this, registry);
+    if (!this.validRelationshipSchema) this.validateRelationships();
+    this.validRelationshipSchema = true;
 
     return (Array.isArray(relations) ? relations : [relations]).reduce((query, relation) => {
       const def = this.relationships[relation];
@@ -169,15 +180,34 @@ export default class BaseModel {
       .first(...fields);
   }
 
-  static get hasMany() {
-    return HAS_MANY;
-  }
+  async save() {
+    const { jsonSchema } = this.constructor;
 
-  static get belongsTo() {
-    return BELONGS_TO;
-  }
+    const modifiedSchema = executeOnDef(
+      this.constructor,
+      'beforeValidate',
+      { ...jsonSchema },
+      this.doc
+    );
+    const schema = modifiedSchema || jsonSchema;
 
-  // static get hasAndBelongsToMany() {
-  //   return HAS_AND_BELONGS_TO_MANY;
-  // }
+    if (schema) {
+      const validate = ajv.compile({ ...schema, type: 'object' });
+      const valid = validate(this.doc);
+      if (!valid) {
+        const { dataPath } = validate.errors[0];
+        const path = dataPath.length ? `\`${dataPath.replace(/^\./, '')}\` ` : '';
+
+        throw new Error(`document ${path}${validate.errors[0].message.replace(/'/g, '`')}`);
+      }
+    }
+
+    executeOnDef(this.constructor, 'beforeSave', this.doc);
+
+    await this.$knex(this.tableName).insert(this.doc);
+
+    return this.$knex(this.tableName)
+      .where({ [this.primaryKey]: this.doc[this.primaryKey] })
+      .first();
+  }
 }
